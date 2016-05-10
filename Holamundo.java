a3c51ce574206b390cf7565ce03fcca587833982
Daniel Ochoa <lennyesteban@hotmail.com>
2016-05-10 18:57:44 -0500

    Modificacion1
    Modificacion 1

--- Holamundo.java
+++ Holamundo.java
@@ -1,6 +1,7 @@
 public class Hello {
   public static void main(String[] args) {
+    //Modificacion Daniel Ochoa
+    System.out.println("Esta es mi parte");
     System.out.println("Hola mundo");
-    System.out.println("Primer colaborador Miguel");
   }
 }
